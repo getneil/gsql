853d1f1ad7ea7fe652f34772c66b362d783bc826
neil molina <get.neilmolina@gmail.com>
2016-06-09 23:53:20 +0800

    expose mock-app gsql models

--- test/app-test.js
+++ test/app-test.js
@@ -3,19 +3,20 @@ var Gsql = require('../'),
     dialect: 'sqlite',
     storage: 'memory'
   });
-const userConfig = require('./Objects/User.js');
-const userProfileConfig = require('./Objects/UserProfile.js');
-const userRoleConfig = require('./Objects/UserRole.js');
 
-const User = gsql.define('User', userConfig);
-const UserProfile = gsql.define('UserRole', userProfileConfig);
-const UserRole = gsql.define('UserRole', userRoleConfig);
+const config = {
+  User:  require('./Objects/User.js'),
+  UserProfile: require('./Objects/UserProfile.js'),
+  UserRole: require('./Objects/UserRole.js'),
+  Team: require('./Objects/Team.js'),
+  Membership: require('./Objects/Membership.js')
+}
 
+const models = {};
+Object.keys(config).forEach((k)=>{
+  models[k] = gsql.define(k, config[k]);
+})
 module.exports = {
   gi: gsql, // GsqlInstance
-  models:{
-    User: User,
-    UserProfile: UserProfile,
-    UserRole: UserRole
-  }
+  models: models
 }
