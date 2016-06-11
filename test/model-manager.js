be56fda61bd4f6922d80d9e40f0fd14044adc14e
neil molina <get.neilmolina@gmail.com>
2016-06-12 00:03:30 +0800

    initialize model-manager

---
+++ test/model-manager.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('ModelManager:',function(){
+
+  describe('should be able to access all the defined models', function(){
+
+  })
+
+  describe('should be able to determine the proper order of sync of objects depending on their dependency', function(){
+
+  })
+
+  describe('should correctly establish relationship among models', function(){
+    /*
+    creates the correct relationship dictionary with its correspond Sequelize relationship
+    */
+  })
+  describe('should create tables if .sync is executed.', function(){
+
+  })
+
+})
